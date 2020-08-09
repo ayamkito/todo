b64666a25ce581f13e8a0ac59dcd20863d66c175
akwotiga kizito <kakwotiga@yahoo.com>
2020-08-09 00:00:12 -0400

    CSS changes and JS progress

--- src/App.js
+++ src/App.js
@@ -13,16 +13,23 @@ class TodoList extends Component {
   }
   addItem = event =>{
     event.preventDefault();
-    console.log("addItem Method fired")
+    console.log("addItem Method fired");
+    this.setState({
+      todos:(this._inputElement.value)
+    })
   }
   render(){
     return (
       <div className="container">
+        <div>
+
         <form onSubmit={this.addItem}>
           <label htmlFor="taskname">Task Name</label>
-          <input onChange={this.handleChange} name="taskname"type="text"placeholder="Add todo here"/>
-          <button type="submit">Add task</button>
+          <input onChange={this.handleChange} name="taskname"type="text"placeholder="Add todo here" ref={(item)=>this._inputElement=item} />
+          <button type="submit">Submit</button>
         </form>
+        </div>
+          {this.state.todos}
       </div>
     )
   }
